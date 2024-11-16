b483cab52c76c5b45ac0504370d172e95880fc24
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-11-16 13:26:32 -0800

    got gradient legent

--- src/components/HeatmapLegend.js
+++ src/components/HeatmapLegend.js
@@ -5,14 +5,20 @@ const HeatmapLegend = () => {
   return (
     <div className="mb-4">
       <h3 className="text-lg font-semibold mb-2">Heatmap Legend</h3>
+      <div className="flex items-center mb-2">
+        <div className="w-16 h-4 border border-black mr-2" style={{
+          background: 'linear-gradient(to right, green, yellow, red)',
+        }}></div>
+        <span>Color Gradient</span>
+      </div>
       <div className="flex space-x-4">
         <div className="flex items-center">
           <div className="w-4 h-4 bg-green-500 border border-black mr-2"></div>
           <span>Similar Pixels</span>
         </div>
         <div className="flex items-center">
-          <div className="w-4 h-4 bg-blue-500 border border-black mr-2"></div>
-          <span>Different Pixels</span>
+          <div className="w-4 h-4 bg-yellow-500 border border-black mr-2"></div>
+          <span>Moderately Different Pixels</span>
         </div>
         <div className="flex items-center">
           <div className="w-4 h-4 bg-red-500 border border-black mr-2"></div>
